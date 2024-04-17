77a1245e08d64442fd412370feae1ff9b694cbf6
josephyi03 <josephcrest29@gmail.com>
2024-04-17 19:37:33 -0400

    balloons

--- project7/app/src/main/java/com/example/project7/Balloons.kt
+++ project7/app/src/main/java/com/example/project7/Balloons.kt
@@ -1,5 +1,7 @@
 package com.example.project7
 
+import kotlinx.coroutines.awaitAll
+
 class Balloons {
 
     private var allBalloons: ArrayList<Balloon>? = null
@@ -29,10 +31,24 @@ class Balloons {
 
     fun hasWon(): Boolean {
 
+        var pops = 0
+        for( balloon in allBalloons!!) {
+            if (balloon.getPopped())
+                pops += 1
+        }
+
+        if (pops == allBalloons!!.size){
+            allPopped = true
+
+            if (counter <= allBalloons!!.size + 3)
+                return true
+        }
+
+        return false
     }
 
     fun hasLost(): Boolean {
-
+        return counter > allBalloons!!.size + 3
     }
 
 
